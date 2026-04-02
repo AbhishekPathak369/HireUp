c2a935c1321b4e389b75cdbfe73f85b960c790e5
AbhishekPathak369 <pathakabhi290@gmail.com>
2026-04-02 16:08:19 +0530

    cors update

--- backend/index.js
+++ backend/index.js
@@ -32,10 +32,10 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-app.use(express.static(path.join(__dirname,"/frontend/dist")))
-app.get('*',(_,res)=>{
-    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
-})
+// app.use(express.static(path.join(__dirname,"/frontend/dist")))
+// app.get('*',(_,res)=>{
+//     res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+// })
 
 app.listen(PORT,()=>{
     connectDB();
