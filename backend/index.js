bb1a13eb01ff580c31e417c421f6d5d3addf27ee
AbhishekPathak369 <pathakabhi290@gmail.com>
2026-04-02 17:20:35 +0530

    l

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: ["https://hireup-plum.vercel.app"],
+    origin: "https://hireup-plum.vercel.app",
     credentials: true
 }
 
